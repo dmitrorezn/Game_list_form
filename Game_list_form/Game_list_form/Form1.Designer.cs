4c553076a043b04ae107486a2db3c3288e3c813c
Дима Резниченко <dmitrorezn@gmail.com>
2020-03-24 02:17:02 +0200

    kk

---
+++ Game_list_form/Game_list_form/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Game_list_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Name = new System.Windows.Forms.Label();
+            this.Description = new System.Windows.Forms.Label();
+            this.Year = new System.Windows.Forms.Label();
+            this.Genre = new System.Windows.Forms.Label();
+            this.Poster = new System.Windows.Forms.Label();
+            this.Req_OS_Architecture = new System.Windows.Forms.Label();
+            this.Req_OS = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Location = new System.Drawing.Point(15, 9);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(45, 17);
+            this.Name.TabIndex = 0;
+            this.Name.Text = "Name";
+            // 
+            // Description
+            // 
+            this.Description.AutoSize = true;
+            this.Description.Location = new System.Drawing.Point(12, 37);
+            this.Description.Name = "Description";
+            this.Description.Size = new System.Drawing.Size(79, 17);
+            this.Description.TabIndex = 1;
+            this.Description.Text = "Description";
+            // 
+            // Year
+            // 
+            this.Year.AutoSize = true;
+            this.Year.Location = new System.Drawing.Point(12, 71);
+            this.Year.Name = "Year";
+            this.Year.Size = new System.Drawing.Size(38, 17);
+            this.Year.TabIndex = 2;
+            this.Year.Text = "Year";
+            // 
+            // Genre
+            // 
+            this.Genre.AutoSize = true;
+            this.Genre.Location = new System.Drawing.Point(12, 106);
+            this.Genre.Name = "Genre";
+            this.Genre.Size = new System.Drawing.Size(48, 17);
+            this.Genre.TabIndex = 3;
+            this.Genre.Text = "Genre";
+            // 
+            // Poster
+            // 
+            this.Poster.AutoSize = true;
+            this.Poster.Location = new System.Drawing.Point(12, 123);
+            this.Poster.Name = "Poster";
+            this.Poster.Size = new System.Drawing.Size(49, 17);
+            this.Poster.TabIndex = 4;
+            this.Poster.Text = "Poster";
+            // 
+            // Req_OS_Architecture
+            // 
+            this.Req_OS_Architecture.AutoSize = true;
+            this.Req_OS_Architecture.Location = new System.Drawing.Point(12, 183);
+            this.Req_OS_Architecture.Name = "Req_OS_Architecture";
+            this.Req_OS_Architecture.Size = new System.Drawing.Size(146, 17);
+            this.Req_OS_Architecture.TabIndex = 5;
+            this.Req_OS_Architecture.Text = "Req_OS_Architecture";
+            // 
+            // Req_OS
+            // 
+            this.Req_OS.AutoSize = true;
+            this.Req_OS.Location = new System.Drawing.Point(12, 154);
+            this.Req_OS.Name = "Req_OS";
+            this.Req_OS.Size = new System.Drawing.Size(62, 17);
+            this.Req_OS.TabIndex = 5;
+            this.Req_OS.Text = "Req_OS";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(418, 210);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 17);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "label6";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(440, 252);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 17);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(403, 167);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(54, 17);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "label10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(385, 123);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(54, 17);
+            this.label11.TabIndex = 9;
+            this.label11.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(403, 88);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(54, 17);
+            this.label12.TabIndex = 8;
+            this.label12.Text = "label12";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(385, 54);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(54, 17);
+            this.label13.TabIndex = 7;
+            this.label13.Text = "label13";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(373, 27);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(54, 17);
+            this.label14.TabIndex = 6;
+            this.label14.Text = "label14";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.Req_OS);
+            this.Controls.Add(this.Req_OS_Architecture);
+            this.Controls.Add(this.Poster);
+            this.Controls.Add(this.Genre);
+            this.Controls.Add(this.Year);
+            this.Controls.Add(this.Description);
+            this.Controls.Add(this.Name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.Label Description;
+        private System.Windows.Forms.Label Year;
+        private System.Windows.Forms.Label Genre;
+        private System.Windows.Forms.Label Poster;
+        private System.Windows.Forms.Label Req_OS_Architecture;
+        private System.Windows.Forms.Label Req_OS;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
---
+++ Game_list_form/Game_list_form/Game.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_list_form
+{
+    class Game
+    {
+        private string Name;
+        private string Description;
+        private uint Year;
+        private string Genre;
+        private Image Poster;
+        private string Req_OS;
+        private string Req_OS_Architecture;
+        private string Req_disk_space;
+
+        private uint Min_Processor_Cores;
+        private uint Min_Processor_Threads;
+        private uint Min_Processor_Clock; // Mhz
+        private uint Min_Graphics_VRAM;
+        private uint Min_Graphics_Gpu_Clock;
+
+        private uint Rec_Processor_Cores;
+        private uint Rec_Processor_Threads;
+        private uint Rec_Processor_Clock; // Mhz
+        private uint Rec_Graphics_VRAM;
+        private uint Rec_Graphics_Gpu_Clock;
+        public Game(string g_name, string g_desc, uint g_year, string g_genre, string g_post_dir, string g_os,
+            string g_osa, string g_ds, uint g_mp_cores, uint g_mp_threads, uint g_mp_clock_mhz, uint g_mg_vram, uint g_mg_clock)
+        {
+            Name = g_name;
+            Description = g_desc;
+            Year = g_year;
+            Genre = g_genre;
+            Poster = Image.FromFile(g_post_dir);
+            Req_OS = g_os;
+            Req_OS_Architecture = g_osa;
+            Req_disk_space = g_ds;
+            Min_Processor_Cores = g_mp_cores;
+            Min_Processor_Threads = g_mp_threads;
+            Min_Processor_Clock = g_mp_clock_mhz;
+            Min_Graphics_VRAM = g_mg_vram;
+            Min_Graphics_Gpu_Clock = g_mg_clock;
+        }
+        public void AddRec(uint g_rp_cores, uint g_rp_threads, uint g_rp_clock_mhz, uint g_rg_vram, uint g_rg_clock)
+        {
+
+        }
+    }
+}
